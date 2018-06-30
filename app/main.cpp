4f204e2fc2bfe0a865946c9192b0c40c1d682146
timonmentink <timon.mentink@demcon.nl>
2018-06-30 15:38:55 +0200

    changed main for coveralls. added coveralls before build

--- app/main.cpp
+++ app/main.cpp
@@ -1,10 +1,13 @@
 #include "UI/MainWindow.h"
 #include <QApplication>
+#include "myfunc.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+
+    fibonachi(2);
     w.show();
 
     return a.exec();
