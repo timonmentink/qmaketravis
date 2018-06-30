eaa74d78e9402eccf0ff928c20a8a95fbba3603a
timonmentink <timon.mentink@demcon.nl>
2018-06-30 13:33:05 +0200

    updated nameing added tests

---
+++ app/myfunc.c
@@ -0,0 +1,25 @@
+
+#include <stdlib.h>
+#include "myfunc.h"
+
+int val;
+
+int fibonachi(int num) {
+    int prev = 1;
+    int next = 1;
+
+    if (num < 0)
+        return 0;
+
+    if (num <= 2)
+        return num;
+
+    int i = 2;
+    while (i < num) {
+        next += prev;
+        prev = next - prev;
+        i++;
+    }
+
+    return next;
+}
---
+++ app/myfunc.h
@@ -0,0 +1,8 @@
+#ifndef MYFUNC_H
+#define MYFUNC_H
+
+int fibonachi(int num);
+
+extern int val;
+
+#endif // MYFUNC_H
